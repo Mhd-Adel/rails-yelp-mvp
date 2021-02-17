6898ad715ff449bc6d90103cbdc2077e4b460478
Mhd Adel Al Masri <mhdadelalmasri@gmail.com>
2021-02-17 17:01:30 +0100

    added routes and controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+dishoom = { name: "Dishoom", address: "8 Boundary St, tawan E2 7JE", phone_number: 123123123, category: "chinese" }
+aboabod = { name: "abo3bod", address: "22 snasd St, mecen E2 7JE", phone_number: 5959595959, category: "italian" }
+symoum = { name: "symoum", address: "66 bukad St, paris E2 7JE", phone_number: 343434343, category: "french" }
+yoro = { name: "yoro", address: "2 ahmad St, honkong E2 7JE", phone_number: 89898989, category: "chinese" }
+sodak = { name: "sodak", address: "4 kaldom St, zarem E2 7JE", phone_number: 676767, category: "belgian" }
+
+restaurants = [dishoom, aboabod, symoum, yoro, sodak]
+
+restaurants.each do |attr|
+  restaurant = Restaurant.create(attr)
+end
